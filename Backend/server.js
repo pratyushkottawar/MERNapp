3b30ece84162319c79020936cb4da156332e8737
pratyushkottawar <pratyushkottawar@gmail.com>
2024-11-03 13:07:24 +0000

    added routes

--- Backend/server.js
+++ Backend/server.js
@@ -1,1 +1,22 @@
 import express from "express";
+import dotenv from 'dotenv';
+
+dotenv.config();
+// express app
+const app = express();
+
+// middleware
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+// routes
+app.get("/", (req, res) => {
+  res.json({ mssg: "Welcome to the app" });
+});
+
+// listen for requests
+app.listen(process.env.PORT, () => {
+  console.log("listening on port", process.env.PORT);
+});
